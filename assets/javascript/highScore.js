d5714b54558472cc8906b9639401d39d965508c4
miao0007 <miao188@hotmail.com>
2020-06-14 03:17:50 +0930

    format files

--- assets/javascript/highScore.js
+++ assets/javascript/highScore.js
@@ -2,25 +2,27 @@
 var scoreDisplayList = document.getElementById("scoreDisplayList");
 var clearScore = document.getElementById("clear");
 var high_scores = JSON.parse(localStorage.getItem("high_scores")) || [];
- displayHighScore();
+displayHighScore();
 
 //  store and display high score
 function displayHighScore() {
-    high_scores = sortScores(high_scores, 'score');
+  high_scores = sortScores(high_scores, "score");
 
-// create high score display list
-    for (var i = 0; i < high_scores.length; i++) {
-      console.log(high_scores[i].score);
-      var li = document.createElement("li"); 
-      var initScore = document.createTextNode(high_scores[i].initials + ": " + high_scores[i].score)  ; 
-      li.appendChild(initScore);
-      scoreDisplayList.appendChild(li);
-    }
+  // create high score display list
+  for (var i = 0; i < high_scores.length; i++) {
+    console.log(high_scores[i].score);
+    var li = document.createElement("li");
+    var initScore = document.createTextNode(
+      high_scores[i].initials + ": " + high_scores[i].score
+    );
+    li.appendChild(initScore);
+    scoreDisplayList.appendChild(li);
+  }
 }
 
 //sort scores from high to low
 function sortScores(array, key) {
-  return array.sort(function(a,b) {
+  return array.sort(function (a, b) {
     if (a.score < b.score) {
       return 1;
     }
@@ -29,7 +31,7 @@ function sortScores(array, key) {
 }
 
 // create clear button can empty score board
-clearScore.addEventListener("click", function() {
-    localStorage.removeItem("high_scores");
-    window.location.reload();
+clearScore.addEventListener("click", function () {
+  localStorage.removeItem("high_scores");
+  window.location.reload();
 });
--- assets/javascript/question.js
+++ assets/javascript/question.js
@@ -1,46 +1,50 @@
-var questions = [{
-    "question": "Inside which HTML element do we put the JavaScript?",
-    "option1": "<js>",
-    "option2": "<javascript>",
-    "option3": "<scripting>",
-    "option4": "<script>",
-    "answer": "<script>"
-},{
-    "question": "How do you write 'Hello World' in an alert box?",
-    "option1": "alertBox('Hello World');",
-    "option2": "msg('Hello World');",
-    "option3": "alert('Hello World');",
-    "option4": "msgBox('Hello World');",
-    "answer": "alert('Hello World');"
-},{
-    "question": "How do you create a function in JavaScript?",
-    "option1": "function = myFunction()",
-    "option2": "function:myFunction()",
-    "option3": "function myFunction()",
-    "option4": "var function = myFunction()",
-    "answer": "function myFunction()"
-
-},{
-    "question": "How to write an IF statement in JavaScript?",
-    "option1": "if i = 5",
-    "option2": "if i = 5 then",
-    "option3": "if i == 5 then",
-    "option4": "if (i == 5)",
-    "answer": "if (i == 5)"
-},{
-    "question": "How does a FOR loop start?",
-    "option1": "for(i = 0; i <= 5; i++)",
-    "option2": "for(i<=5; i++)",
-    "option3": "for i = 1 to 5",
-    "option4": "for(i = 0; i <= 5)",
-    "answer": "for(i = 0; i <= 5; i++)"
-},{
-    "question": "Which event occurs when the user clicks on an HTML element?",
-    "option1": "onmouseclick",
-    "option2": "onchange",
-    "option3": "onclick",
-    "option4": "onmouseover",
-    "answer": "onclick"
-}
-
-]
+var questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    option1: "<js>",
+    option2: "<javascript>",
+    option3: "<scripting>",
+    option4: "<script>",
+    answer: "<script>",
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    option1: "alertBox('Hello World');",
+    option2: "msg('Hello World');",
+    option3: "alert('Hello World');",
+    option4: "msgBox('Hello World');",
+    answer: "alert('Hello World');",
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    option1: "function = myFunction()",
+    option2: "function:myFunction()",
+    option3: "function myFunction()",
+    option4: "var function = myFunction()",
+    answer: "function myFunction()",
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    option1: "if i = 5",
+    option2: "if i = 5 then",
+    option3: "if i == 5 then",
+    option4: "if (i == 5)",
+    answer: "if (i == 5)",
+  },
+  {
+    question: "How does a FOR loop start?",
+    option1: "for(i = 0; i <= 5; i++)",
+    option2: "for(i<=5; i++)",
+    option3: "for i = 1 to 5",
+    option4: "for(i = 0; i <= 5)",
+    answer: "for(i = 0; i <= 5; i++)",
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    option1: "onmouseclick",
+    option2: "onchange",
+    option3: "onclick",
+    option4: "onmouseover",
+    answer: "onclick",
+  },
+];
